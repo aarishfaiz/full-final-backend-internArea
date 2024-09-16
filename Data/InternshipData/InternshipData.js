e5bee6ae046cd7514074bdb534324c5eddc84402
Aarish Faiz <104681243+aarishfaiz@users.noreply.github.com>
2024-09-16 17:45:35 +0530

    Add files via upload

---
+++ Data/InternshipData/InternshipData.js
@@ -0,0 +1,61 @@
+
+
+const internshipData = [
+  {
+    id: 1,
+    title: "Data Science",
+    company: "Microsoft",
+    location: "Hydrabad",
+    duration: "2 months",
+    stipend: "100000",
+    type: "Internship",
+    aboutCompany: "Microsoft is a global technology leader known for its innovative software, hardware, and cloud services. With a rich history of shaping the digital landscape, Microsoft continues to empower individuals and organizations worldwide with cutting-edge solutions and tools.",
+    whoCanApply: "Students pursuing a degree in Computer Science",
+  },
+  {
+    id: 2,
+    title: "Full Stack Developer",
+    company: "Apple",
+    location: "Mumbai",
+    duration: "4 months",
+    stipend: "1,50000",
+    type: "Internship",
+    aboutCompany: "Apple, a pioneer in consumer electronics, is renowned for its iconic products like the iPhone, iPad, and MacBook. With a focus on innovation and design, Apple continues to redefine the way we interact with technology, offering seamless integration across its ecosystem.",
+    whoCanApply: "Students pursuing a degree in Computer Science",
+  },
+  {
+    id: 3,
+    title: "Java Developer",
+    company: "Amazon",
+    location: "Pune",
+    duration: "6 months",
+    stipend: "1,50000",
+    type: "Internship",
+    aboutCompany: "Amazon, the global e-commerce giant, revolutionizes shopping with its vast selection, fast delivery, and innovative services like Amazon Prime. It's also a leader in cloud computing through Amazon Web Services (AWS), powering businesses worldwide.",
+    whoCanApply: "Students pursuing a degree in Computer Science",
+  },
+  {
+    id: 4,
+    title: "Backend Developer",
+    company: "Netflix",
+    location: "Delhi",
+    duration: "1 months",
+    stipend: "100000",
+    type: "Internship",
+    aboutCompany: "Netflix, the premier streaming service, offers a vast library of movies, TV shows, and original content, redefining entertainment consumption worldwide. With its user-friendly interface and personalized recommendations, it has become a household staple for entertainment seekers.",
+    whoCanApply: "Students pursuing a degree in Computer Science",
+  },
+  {
+    id: 5,
+    title: "Android Developer",
+    company: "Google",
+    location: "Pune",
+    duration: "6 months",
+    stipend: "1,20000",
+    type: "Internship",
+    aboutCompany: "Google, the tech giant, pioneers innovation across diverse sectors, from search engines to cloud computing. With its user-centric approach and cutting-edge technology, Google continues to shape the digital landscape, empowering users and businesses worldwide.",
+    whoCanApply: "Students pursuing a degree in Computer Science",
+  }
+  
+];
+
